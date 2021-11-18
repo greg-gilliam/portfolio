9b856191772bec2b264c014dc5e8605e553593de
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-11-17 19:24:20 -0800

    basic setup started

---
+++ src/Thumbnail.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
+
+function Thumbnail(props) {
+  return (
+    <div className="project">
+      <Link to="">
+        <div className="project-image">
+          <img src="" alt="Projects" />
+        </div>
+        <div className="project-title">""</div>
+        <div className="project-category">""</div>
+      </Link>
+    </div>
+  );
+}
+
+export default Thumbnail;
