361ee946231b7c7359781bccdbe3cf468cbca638
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2023-05-27 15:36:08 -0700

    About.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
-import logo from "./logo.svg";
+import React from 'react';
 import "./App.css";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-import About from "./About.js";
-import Projects from "./Projects.js";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Route exact path="/" component={Projects} />
-        <Route path="/about" component={About} />
-
-        <div className="navigation"></div>
-        <img src={logo} className="logo" alt="logo" />
-
-        <div className="navigation-sub">
-          <Link to="/" className="item">
-            Projects
-          </Link>
-          <Link to="/about" className="item">
-            About
-          </Link>
-        </div>
-      </div>
-    </BrowserRouter>
   );
 }
 
