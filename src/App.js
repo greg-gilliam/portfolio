df7bad24e64904aaf0a6c2225235db18038f9ac3
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2023-05-27 18:11:06 -0700

    App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import "./App.css";
-
-function App() {
-  return (
-  );
+import React, { Component } from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+import Resume from "./components/Resume";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import resumeData from "./resumeData";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header resumeData={resumeData} />
+        <About resumeData={resumeData} />
+        <Resume resumeData={resumeData} />
+        <Projects resumeData={resumeData} />
+        <Contact resumeData={resumeData} />
+        <Footer resumeData={resumeData} />
+      </div>
+    );
+  }
 }
-
 export default App;
